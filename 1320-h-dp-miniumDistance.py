01577370ae5902a168c27e6a8db19deeaf088387
RenxuanCloud <RenxuanCloud@mail>
2021-11-12 13:09:56 +0800

    1

--- 1320-h-dp-miniumDistance.py
+++ 1320-h-dp-miniumDistance.py
@@ -158,6 +158,57 @@ class Solution:
         return min(stepLenth(i,word) for i in range(1,len(word)))
         return resultLenth(0,1,word)
             
+
+    def minimumDistanceLocalOptimum(self, word: str) -> int:
+        loc = {
+            'A':(0,0),
+            'B':(0,1),
+            'C':(0,2),
+            'D':(0,3),
+            'E':(0,4),
+            'F':(0,5),
+
+            'G':(1,0),
+            'H':(1,1),
+            'I':(1,2),
+            'J':(1,3),
+            'K':(1,4),
+            'L':(1,5),
+
+            'M':(2,0),
+            'N':(2,1),
+            'O':(2,2),
+            'P':(2,3),
+            'Q':(2,4),
+            'R':(2,5),
+
+            'S':(3,0),
+            'T':(3,1),
+            'U':(3,2),
+            'V':(3,3),
+            'W':(3,4),
+            'X':(3,5),
+
+            'Y':(4,0),
+            'Z':(4,1)
+        }
+        def distance(charA,charB):
+            a = loc[charA]
+            b = loc[charB]
+            return abs(a[0] - b[0])+abs(a[1] -b[1])
+
+        """
+        dp[i][j]
+        dp[i][j] = min(dp[i-1][j]+distance(i-1,i),dp[i][j-1]+distance(j-1,j))
+        
+        """
+        n = len(word)
+        dp = [[0]*(n+1) for _ in range(n+1)]
+        for j in range(n):
+            i = 0
+            while i !=n or j!=n:
+                if 
+
 s = Solution()
 print(s.minimumDistance("AXFX"))#局部最优
 print(s.minimumDistance("ABYZ"))#局部最优
---
+++ autosubmit.py
@@ -0,0 +1,73 @@
+import os
+import time
+import subprocess
+import argparse
+ 
+class TimeoutError(Exception):
+    pass 
+ 
+def excuteCmd2(local,repos, timeout = 1):
+
+        
+        res = os.popen('git init')
+        output_str = res.read()   # 获得输出字符串
+        print(output_str)
+        time.sleep(2)
+        res = os.popen('git add .')
+        output_str = res.read()   # 获得输出字符串
+        print(output_str)
+        time.sleep(2)
+        res = os.popen('git remote add  origin '+repos)
+        output_str = res.read()   # 获得输出字符串
+        print(output_str)
+        time.sleep(2)
+        res = os.popen('git push origin master')
+        output_str = res.read()   # 获得输出字符串
+        print(output_str)
+        time.sleep(2)
+        res = os.popen('yes')
+        output_str = res.read()   # 获得输出字符串
+        print(output_str)
+        time.sleep(2)
+        res = os.popen('exit')
+        output_str = res.read()   # 获得输出字符串
+        print(output_str)
+        time.sleep(2)
+        return
+def excuteCmd(local,repos, timeout = 1):
+        s = subprocess.Popen("C:\Windows\System32\cmd.exe",stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell = True)
+        #out, err =s.communicate('cd '+local+'\n')
+        #print(out)
+        time.sleep(2)
+        s.stdin.write(str.encode('git init \n'))
+        #print(s.stdout.write())
+        time.sleep(2)
+        s.stdin.write(str.encode('git add . \n'))
+        #print(s.stdout.write())
+        time.sleep(2)
+        s.stdin.write(str.encode('git remote add  origin git@github.com:Ren-Xuan/LeetCode.git \n'))
+        #print(s.stdout.write())
+        time.sleep(2)
+        s.stdin.write(str.encode('git push origin master\n'))
+        #print(s.stdout.write())
+        time.sleep(2)
+        out, err =s.communicate(str.encode('yes\n'))
+        print(out.decode(encoding='gbk'))
+        time.sleep(2)
+        #s.stdin.write(str.encode('exit\n'))
+        #print(s.stdout.write())
+        #time.sleep(2)
+        s.kill()
+    
+ 
+if __name__ == '__main__':
+        ''' self test ''' 
+        parser = argparse.ArgumentParser(description='manual to this script')
+        parser.add_argument('--local', type=str,default=None)
+        parser.add_argument('--repos', type=str,default=None)
+        args = parser.parse_args()
+        try: 
+            ret = excuteCmd(args.local,args.repos,2)
+            print(ret) 
+        except TimeoutError: 
+            print("error")
