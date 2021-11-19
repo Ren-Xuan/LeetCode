ee3627a2fe3e02da6c4236a2758b369175813649
tencencloud <1584160851@qq.com>
2021-11-20 02:30:41 +0800

    2021-11-20 02:30:36

---
+++ 594-e-finLHS.py
@@ -0,0 +1,28 @@
+
+from typing import Counter
+
+
+class Solution:
+    def findLHS(self, nums) -> int:
+        nums.sort()
+        l = 0
+        r = 0
+        res = 0
+        while r<len(nums):
+            while nums[r] - nums[l]>1:
+                l+=1
+            if nums[r]-nums[l]==1:
+                res = max(res,r-l+1)
+            r+=1
+        return res
+    def findLHS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0
+        d = Counter(nums)
+        for num in nums:
+            if num + 1 in d:
+                ans = max(ans, d[num] + d[num + 1])
+        return ans
