082de32ab78b1eb265b342d66d4eb3f4ee1b6d2a
tencencloud <1584160851@qq.com>
2021-12-11 02:30:51 +0800

    2021-12-11 02:30:46

---
+++ 911-m-TopVotedCandidate.py
@@ -0,0 +1,22 @@
+from typing import Counter, List
+import heapq
+class TopVotedCandidate:
+
+    def __init__(self, persons: List[int], times: List[int]):
+        q = []
+        self.res = []
+        c = Counter()
+        for i, j in zip(persons, times):
+            c[i] += 1
+            heapq.heappush(q, (-c[i], -j, i))
+            self.res.append((q[0][2], -q[0][1]))
+
+    def q(self, t: int) -> int:
+        l, r = 1, len(self.res)
+        while l < r:
+            m = l + r >> 1
+            if self.res[m][1] <= t:
+                l = m + 1
+            else:
+                r = m
+        return self.res[l - 1][0]
